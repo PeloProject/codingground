902791349e7c029d9bdcfc63faadd5419df8a606
PeloProject <peko.12.31.love.4.03@gmail.com>
2017-11-04 12:29:01 +0900

    Update main.cpp

--- New Project-20171103/main.cpp
+++ New Project-20171103/main.cpp
@@ -1,11 +1,19 @@
 #include <iostream>
+#include "class/Components.cpp"
 
 using namespace std;
 
+Manager gManager;
+auto& newPlayer(gManager.AddEntity());
+
 int main()
 {
-   cout << "Hello World" << endl; 
+    newPlayer.AddComponent<PositionComponent>();
+    gManager.Update();
+    cout << newPlayer.GetComponent<PositionComponent>().x() << "," << newPlayer.GetComponent<PositionComponent>().y() << endl; 
+    cout << "Hello World" << endl; 
    
-   return 0;
+    return 0;
 }
+
 
